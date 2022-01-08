fd6dee8fdbd176f9eaaec1da3d7f39752a14bb99
jerjjj <93431283+jerjjj@users.noreply.github.com>
2022-01-09 00:09:58 +0800

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,6 @@
  ​#​ SPDX-License-Identifier: Apache-2.0 
  ​# 
   
- ​add_lunch_combo twrp_merlin-user 
- ​add_lunch_combo twrp_merlin-userdebug 
- ​add_lunch_combo twrp_merlin-eng
+ ​add_lunch_combo omin_merlin-user 
+ ​add_lunch_combo omin_merlin-userdebug 
+ ​add_lunch_combo omin_merlin-eng
